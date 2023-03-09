54cc34cb2fd21d60c106c29c46a834b49c025de2
Walmir <owalmirneto@gmail.com>
2023-03-08 23:11:08 -0300

    Load I18n locales files (#2)

--- lib/form_ease.rb
+++ lib/form_ease.rb
@@ -4,3 +4,6 @@ require "simple_form"
 
 require "form_ease/engine"
 require "form_ease/error"
+
+files = Dir[File.join(File.dirname(__FILE__), "../config/locales/**/*.{rb,yml}")]
+I18n.load_path.concat(files)
